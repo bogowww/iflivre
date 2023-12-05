a296707c13bed80b55039a6659a90f6d3f0d6a4d
Pedro <Pedro@hdeifler>
2023-12-05 12:22:13 -0300

    e

--- config/config.inc.php
+++ config/config.inc.php
@@ -2,7 +2,7 @@
 define ('MYSQL_HOST','localhost');
 define ('MYSQL_USUARIO','root');
 define ('MYSQL_SENHA','');
-define ('MYSQL_DB','SIGGMA');
+define ('MYSQL_DB','iflivre');
 define ('MYSQL_PORT','3306');
 
 define ('MYSQL_DSN',"mysql:host=".MYSQL_HOST.";port=".MYSQL_PORT.";dbname=".MYSQL_DB.";charset=UTF8");
